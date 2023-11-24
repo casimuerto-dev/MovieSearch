b5f86805e09ad6c217a0aec08a9de72d81d4bbfc
IVAN <ivacesize@gmail.com>
2023-11-24 15:11:06 -0500

    feat: added a little css

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -4,7 +4,16 @@ export function MovieCard(props: MovieObj) {
   return (
     <div className="movieCard">
       <h3>{props.Title}</h3>
-      <img className="posterImage" src={props.Poster} />
+      <div className="imageDiv">
+        <img
+          className="posterImage"
+          src={
+            props.Poster !== "N/A"
+              ? props.Poster
+              : "https://auctions.leski.com.au/images/lot/6892/68920_xl.jpg?1692887757"
+          }
+        />
+      </div>
       <p>Year {props.Year}</p>
     </div>
   );
