ab9ad720308a1baeb967958c8bc7a1d3df5aa10d
IVAN <ivacesize@gmail.com>
2024-01-15 08:27:43 -0500

    fix: secure api url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL = `http://www.omdbapi.com?apikey=${API_KEY}&`;
+const BASE_URL = `https://www.omdbapi.com?apikey=${API_KEY}&`;
 
 export { API_KEY, BASE_URL };
